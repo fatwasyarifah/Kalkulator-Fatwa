4a74970dc4c2e5f6d3eff53ae72361356a3b8848
Fatwa Syarifah <39793690+fatwasyarifah@users.noreply.github.com>
2020-05-13 23:03:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,115 +1,117 @@
+/** Menampilkan NOMOR pada screen */
 const calculatorScreen = document.querySelector(".calculator-screen")
 
 const updateScreen = (number) => {
-  calculatorScreen.value = number
+    calculatorScreen.value = number
 }
 
 const numbers = document.querySelectorAll(".number")
 
 numbers.forEach((number) => {
-  number.addEventListener("click", (event) => {
-    console.log(event.target.value)
-  })
+    number.addEventListener("click", (event) => {
+        updateScreen (event.target.value)
+    })
 })
 
+/** Membedakan bilangan 1 dengan bilangan 2 */
 let prevNumber = ''
-let calculationOperator = ''
-let currentNumber = '0'
+let calculationperator = ''
+let currentNumber= '0'
 
 const inputNumber = (number) => {
-  if (currentNumber === '0') {
-    currentNumber = number
-  } else {
-    currentNumber += number
-  }
+    if (currentNumber === '0') {
+        currentNumber = number
+    }else {
+        currentNumber += number
+    }
 }
 
 numbers.forEach((number) => {
-  number.addEventListener("click", (event) => {
-    inputNumber(event.target.value)
-    updateScreen(currentNumber)
-  })
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen (currentNumber)
+    })
 })
 
-const operators = document.querySelectorAll(".operator")
+/** Memasukan Operator */
+const operators = document.querySelectorAll (".operator")
 
-operators.forEach((operator) => {
-  operator.addEventListener("click", (event) => {
-    inputOperator(event.target.value)
-  })
+operators.forEach ((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator (event.target.value)
+    })
 })
 
 const inputOperator = (operator) => {
-  if (calculationOperator === '') {
+    if (calculationperator === '') {
     prevNumber = currentNumber
-  }
-  calculationOperator = operator
-  currentNumber = '0'.
+    }
+    calculationperator = operator
+    currentNumber = '0'
 }
 
+/** Menghitung Bilanga dengan pilihan tombol operator */
+const calculate = () => {
+    let result = ''
+    switch (calculationperator){
+        case '+':
+            result = (parseFloat(prevNumber)*10)/10 + (parseFloat(currentNumber)*10)/10
+            break;
+        case '-':
+            result = prevNumber - currentNumber
+            break;
+        case '/':
+            result = prevNumber / currentNumber
+            break;
+        case '*':
+            result = prevNumber * currentNumber
+            break;
+        default:
+            return;
+    }
+    currentNumber = result
+    calculationperator = ''
+}
+
+/** SAMA DENGAN */
 const equalSign = document.querySelector('.equal-sign')
 
 equalSign.addEventListener('click', () => {
-  calculate()
-  updateScreen(currentNumber)
+    calculate()
+    updateScreen (currentNumber)
 })
 
-const calculate = () => {
-  let result = ''
-  switch (calculationOperator) {
-    case "+":
-      result = (parseFloat(+prevNumber)*10 + parseFloat(+currentNumber)*10)/10
-      break
-    case "-":
-      result = (parseFloat(prevNumber)*10 - parseFloat(currentNumber)*10)/10
-      break
-    case "*":
-      result = (parseFloat(prevNumber)*10 * parseFloat(currentNumber)*10)/100
-      break
-    case "/":
-      result = parseFloat(prevNumber) / parseFloat(currentNumber)
-      break
-    default:
-      return
-  }
-  currentNumber = result
-  calculationOperator = ''
-}
-
-const clearAll = () =>  {
-  prevNumber = ''
-  calculationOperator = ''
-  currentNumber = '0'
+/** Clear  */
+const clearAll = () => {
+    prevNumber= ''
+    calculationOperator = ''
+    currentNumber = '0'
 }
 
 const clearBtn = document.querySelector('.all-clear')
 
 clearBtn.addEventListener('click', () => {
-  clearAll()
-  updateScreen(currentNumber)
+    clearAll ()
+    updateScreen (currentNumber)
 })
+
+/** DESIMAL */
 
 inputDecimal = (dot) => {
-  if (currentNumber.includes('.')) {
-    return
-  }
-  currentNumber += dot
+    if (currentNumber.includes ('.')){
+        return
+    }
+    currentNumber += dot
 }
 
-const decimal = document.querySelector('.decimal')
+const decimal = document.querySelector ('.decimal')
 
-decimal.addEventListener('click', (event) => {
-  inputDecimal(event.target.value)
-  updateScreen(currentNumber)
+decimal.addEventListener ('click', (event) =>{
+    inputDecimal (event.target.value)
+    updateScreen (currentNumber)
 })
 
-const persen = document.querySelector (".percentage")
-
+const persen = document.querySelector (".persen")
 persen.addEventListener ("click", (event) => {
-    inputPersen(event.target.value)
-    updateScreen(currentNumber)
+    calculatorScreen.value = currentNumber / 100       
 })
-
-const inputPersen = () => {
-    currentNumber /= 100
-}
